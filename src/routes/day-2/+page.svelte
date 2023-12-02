f7a8d886ce88892e3af31a4bc1e7b1c98c074b2b
Edwards Presume <edwardspresume@gmail.com>
2023-12-02 16:01:24 -0500

    Add cookie counter functionality to Day 2 page

---
+++ src/routes/day-2/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import H1 from '$components/H1.svelte';
+	import Button from '$components/ui/button/button.svelte';
+
+	import { Minus, PlusIcon } from 'lucide-svelte';
+
+	let cookieCounter = $state(0);
+</script>
+
+<H1>Day 2</H1>
+
+<p>Cookie munched: {cookieCounter}</p>
+
+<div class="flex gap-2 mt-5">
+	<Button size="icon" onclick={() => cookieCounter++}>
+		<PlusIcon />
+	</Button>
+
+	<Button size="icon" onclick={() => cookieCounter--}>
+		<Minus />
+	</Button>
+
+	<Button onclick={() => (cookieCounter = 0)}>Reset</Button>
+</div>
