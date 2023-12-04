36696aeaa4ffcdbeef9007e9862d21a9a5f5b8ab
Edwards Presume <edwardspresume@gmail.com>
2023-12-04 11:29:02 -0500

    style(app.pcss): add font-mono class to body to change font style to monospace feat(santa-svelte.png): add new image asset for Svelte Santa logo feat(SiteMainHeader.svelte): add Svelte Santa logo to the site main header

--- src/lib/components/siteMainHeader/SiteMainHeader.svelte
+++ src/lib/components/siteMainHeader/SiteMainHeader.svelte
@@ -3,13 +3,17 @@
 
 	import Button from '$components/ui/button/button.svelte';
 	import ThemeToggler from './ThemeToggler.svelte';
+
+	import svelteSanta from '$lib/assets/santa-svelte.png?enhanced';
 </script>
 
 <header class="relative mb-10 border-b">
 	<div class="container flex items-center justify-between p-2">
-		<Button href={PAGES['/']} variant="link" class="text-2xl font-bold"
-			>Advent Of Svelte 2023</Button
-		>
+		<Button href={PAGES['/']} variant="link" class="text-2xl font-bold">
+			<enhanced:img src={svelteSanta} alt="Svelte Santa logo" class="object-contain w-10 h-10" />
+
+			<span> Advent Of Svelte 2023 </span>
+		</Button>
 
 		<ThemeToggler />
 	</div>
