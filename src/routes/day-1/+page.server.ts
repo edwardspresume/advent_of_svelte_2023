da6405924446a6ba71eefc758f9469c009da18f2
Edwards Presume <edwardspresume@gmail.com>
2023-12-04 14:28:12 -0500

    Refactor loading and categorizing advent data

--- src/routes/day-1/+page.server.ts
+++ src/routes/day-1/+page.server.ts
@@ -5,32 +5,41 @@ type JsonData = {
 	tally: number;
 };
 
+type NaughtyOrNiceEntry = JsonData & {
+	category: 'Nice' | 'Naughty';
+};
+
 export const load = (async ({ fetch }) => {
+	let naughty_or_nice_list: NaughtyOrNiceEntry[] = [];
+
 	try {
 		const adventDataResponse = await fetch(
 			'https://advent.sveltesociety.dev/data/2023/day-one.json'
 		);
+
 		if (!adventDataResponse.ok) {
-			throw new Error('Failed to fetch advent data');
+			throw new Error(`Failed to fetch advent data: ${adventDataResponse.statusText}`);
 		}
 
 		const adventDataJson = (await adventDataResponse.json()) as JsonData[];
 
-		const listForNaughtyChildren = adventDataJson
-			.filter((child) => child.tally < 0)
-			.sort((a, b) => a.tally - b.tally);
-
-		const listForNiceChildren = adventDataJson
-			.filter((child) => child.tally >= 0)
+		naughty_or_nice_list = adventDataJson
+			.map(
+				(child): NaughtyOrNiceEntry => ({
+					...child,
+					category: child.tally >= 0 ? 'Nice' : 'Naughty'
+				})
+			)
 			.sort((a, b) => b.tally - a.tally);
 
 		return {
-			naughty_or_nice_list: { naughty: listForNaughtyChildren, nice: listForNiceChildren }
+			naughty_or_nice_list
 		};
 	} catch (error) {
 		console.error('Error loading advent data:', error);
+
 		return {
-			naughty_or_nice_list: { naughty: [], nice: [] }
+			naughty_or_nice_list
 		};
 	}
 }) satisfies PageServerLoad;
