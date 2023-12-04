02be5e7a22049ee1351dfea0d3fa9bc95fd0da0e
Edwards Presume <edwardspresume@gmail.com>
2023-12-04 09:06:20 -0500

    chore(+page.svelte): add navigation buttons for each day of Advent of Code challenges feat(+page.svelte): add introductory paragraph explaining the purpose of the page and linking to the Advent of Code website

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,28 @@
 <script>
+	import { route } from '$lib/ROUTES';
+
 	import H1 from '$components/H1.svelte';
+	import Button from '$components/ui/button/button.svelte';
 
 	export let data;
+
+	const days = [
+		{ name: 'Day 1', path: route('/day-1') },
+		{ name: 'Day 2', path: route('/day-2') }
+	];
 </script>
 
 <H1>Svelte Advent of Code Challenges</H1>
+
+<p class="mb-4 text-lg text-center">
+	These are my solutions to the <a
+		class="text-blue-500 transition duration-300 hover:text-blue-600"
+		href="https://advent.sveltesociety.dev/">Advent of Code 2023</a
+	> challenges.
+</p>
+
+<nav class="flex items-center justify-center gap-4">
+	{#each days as day}
+		<Button class="px-2 bg-blue-500 hover:bg-blue-600" href={day.path}>{day.name}</Button>
+	{/each}
+</nav>
