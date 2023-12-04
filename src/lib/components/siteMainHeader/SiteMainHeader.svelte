d6f57a2cc79e6d52fc887003d35951c7981bf29a
Edwards Presume <edwardspresume@gmail.com>
2023-12-04 09:06:09 -0500

    Add Button component to SiteMainHeader

--- src/lib/components/siteMainHeader/SiteMainHeader.svelte
+++ src/lib/components/siteMainHeader/SiteMainHeader.svelte
@@ -1,13 +1,16 @@
 <script>
 	import { route } from '$lib/ROUTES';
 
+	import Button from '$components/ui/button/button.svelte';
 	import SiteMainNav from './SiteMainNav.svelte';
 	import ThemeToggler from './ThemeToggler.svelte';
 </script>
 
 <header class="relative mb-10 border-b">
 	<div class="container flex items-center justify-between p-2">
-		<a href={route('/')} class="text-2xl font-bold">Advent Of Svelte 2023</a>
+		<Button href={route('/')} variant="link" class="text-2xl font-bold"
+			>Advent Of Svelte 2023</Button
+		>
 
 		<div class="flex gap-2">
 			<ThemeToggler />
