87d5bfa4c61f0c583f711c0da125d29153dd2c44
Edwards Presume <edwardspresume@gmail.com>
2023-12-05 16:57:19 -0500

    feat(page.svelte): add toast notification when a child is added to the list feat(page.svelte): add support for adding a child's tally as a string and convert it to a number feat(page.svelte): add support for tracking the number of children added by the user feat(page.svelte): reset addedByYou count when the reset button is clicked feat(page.svelte): reset newChildName and newChildTally values when a child is added to the list feat(page.svelte): update ChildrenStatisticsSection component to display the number of children added by the user in the addedByYou card

--- src/routes/day-1/+page.svelte
+++ src/routes/day-1/+page.svelte
@@ -2,6 +2,8 @@
 	import { writable } from 'svelte/store';
 	import type { PageData } from './$types';
 
+	import { toast } from 'svelte-sonner';
+
 	import H1 from '$components/H1.svelte';
 	import InputField from '$components/form/InputField.svelte';
 	import Button from '$components/ui/button/button.svelte';
@@ -12,21 +14,29 @@
 
 	const naughty_and_nice_list_store = writable(data.naughty_and_nice_list);
 
-	$: nicestChild = $naughty_and_nice_list_store.sort((a, b) => b.tally - a.tally)[0];
-	$: naughtiestChild = $naughty_and_nice_list_store.sort((a, b) => a.tally - b.tally)[0];
+	$: nicestChild = [...$naughty_and_nice_list_store].sort((a, b) => b.tally - a.tally)[0];
+	$: naughtiestChild = [...$naughty_and_nice_list_store].sort((a, b) => a.tally - b.tally)[0];
 
 	let newChildName: string;
-	let newChildTally: number = 210;
+	let newChildTally: string;
+	let addedByYou = 0;
 
 	function addChild() {
 		$naughty_and_nice_list_store = [
 			...$naughty_and_nice_list_store,
 			{
 				name: newChildName,
-				tally: newChildTally,
-				category: newChildTally >= 0 ? 'nice' : 'naughty'
+				tally: +newChildTally,
+				category: +newChildTally >= 0 ? 'nice' : 'naughty'
 			}
 		];
+
+		addedByYou += 1;
+
+		toast.success(`Added ${newChildName} to the list!`);
+
+		newChildName = '';
+		newChildTally = '';
 	}
 </script>
 
@@ -35,26 +45,35 @@
 {#if $naughty_and_nice_list_store.length === 0}
 	<p class="text-center">You haven't added any children yet.</p>
 {:else}
-	<ChildrenStatisticsSection
-		{nicestChild}
-		{naughtiestChild}
-		totalChildren={$naughty_and_nice_list_store.length}
-	/>
+	<div class="grid gap-8">
+		<ChildrenStatisticsSection
+			{addedByYou}
+			{nicestChild}
+			{naughtiestChild}
+			totalChildren={$naughty_and_nice_list_store.length}
+		/>
 
-	<div class="flex justify-between gap-2">
-		<form
-			class="grid items-center gap-2 mb-4 grid-cols-[repeat(auto-fit,minmax(200px,1fr))]"
-			on:submit|preventDefault={addChild}
-		>
-			<InputField type="text" placeholder="Name" bind:value={newChildName} />
+		<section class="flex flex-col justify-between gap-3 sm:flex-row">
+			<form
+				class="flex flex-col w-full gap-3 sm:flex-row sm:items-center"
+				on:submit|preventDefault={addChild}
+			>
+				<InputField type="text" placeholder="Name" bind:value={newChildName} required />
+				<InputField type="number" placeholder="Tally" bind:value={newChildTally} required />
 
-			<Button size="sm" class="w-full" type="submit">Add Child</Button>
-		</form>
+				<Button size="sm" type="submit">Add Child</Button>
+			</form>
 
-		<Button on:click={() => ($naughty_and_nice_list_store = data.naughty_and_nice_list)}>
-			Reset
-		</Button>
+			<Button
+				on:click={() => {
+					addedByYou = 0;
+					$naughty_and_nice_list_store = data.naughty_and_nice_list;
+				}}
+			>
+				Reset
+			</Button>
+		</section>
+
+		<NaughtyAndNiceTable {naughty_and_nice_list_store} />
 	</div>
-
-	<NaughtyAndNiceTable {naughty_and_nice_list_store} />
 {/if}
--- src/routes/day-1/lib/components/ChildrenStatisticsSection.svelte
+++ src/routes/day-1/lib/components/ChildrenStatisticsSection.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
-	import type { NaughtyOrNiceEntry } from '$lib/types';
+	import type { NaughtyOrNiceEntry } from '../types';
 
 	import Card from './Card.svelte';
 
+	export let addedByYou: Number;
 	export let totalChildren: Number;
 	export let nicestChild: NaughtyOrNiceEntry;
 	export let naughtiestChild: NaughtyOrNiceEntry;
 </script>
 
-<section class="grid gap-6 mb-10 grid-cols-[repeat(auto-fit,minmax(220px,1fr))]">
+<section class="grid gap-6 grid-cols-[repeat(auto-fit,minmax(220px,1fr))]">
 	<Card>
 		<svelte:fragment slot="title">Total Children</svelte:fragment>
 
@@ -50,6 +51,6 @@
 			<iconify-icon icon="gala:add" class="text-green-500"></iconify-icon>
 		</svelte:fragment>
 
-		<svelte:fragment slot="data">0</svelte:fragment>
+		<svelte:fragment slot="data">{addedByYou}</svelte:fragment>
 	</Card>
 </section>
