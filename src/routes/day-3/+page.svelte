5e54faab9b079b6b049cb76b1050b24b0abdcc70
Edwards Presume <edwardspresume@gmail.com>
2023-12-08 17:05:25 -0500

    feat(day-3): add GiftStatisticsSection component to display statistics about children gifts
    - Added `GiftStatisticsSection` component to display statistics about children gifts.
    - The component receives `totalChildren`, `totalWeight`, and `tripsCreated` as props.
    - The component uses the `Card` component to display each statistic.
    - The `totalChildren` statistic displays the total number of children waiting for gifts.
    - The `totalWeight` statistic displays the total weight of gifts for the children.
    - The `tripsCreated` statistic displays the number of trips created by the user.

--- src/routes/day-3/+page.svelte
+++ src/routes/day-3/+page.svelte
@@ -1,5 +1,28 @@
+<script context="module" lang="ts">
+	import { writable } from 'svelte/store';
+	import type { ChildGiftType } from './lib/types';
+
+	export const childrenGiftsStore = writable<ChildGiftType[]>([]);
+</script>
+
 <script lang="ts">
 	import type { PageData } from './$types';
 
+	import H1 from '$components/H1.svelte';
+	import GiftStatisticsSection from './lib/components/GiftStatisticsSection.svelte';
+
 	export let data: PageData;
+
+	childrenGiftsStore.set(data.childrenGifts);
+
+	$: totalChildren = $childrenGiftsStore.length;
+	$: totalWeight = Number(
+		$childrenGiftsStore.reduce((acc, curr) => acc + curr.weight, 0).toFixed(2)
+	);
+
+	let tripsCreated = 0;
 </script>
+
+<H1>Day 3: Sled Load Balancer</H1>
+
+<GiftStatisticsSection {totalChildren} {totalWeight} {tripsCreated} />
---
+++ src/routes/day-3/lib/components/GiftStatisticsSection.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import Card from '$components/Card.svelte';
+
+	export let totalChildren: number;
+	export let totalWeight: number;
+	export let tripsCreated: number;
+</script>
+
+<section class="grid gap-6 grid-cols-[repeat(auto-fit,minmax(220px,1fr))]">
+	<Card>
+		<svelte:fragment slot="title">Total Children</svelte:fragment>
+
+		<svelte:fragment slot="icon">
+			<iconify-icon icon="twemoji:wrapped-gift" width="22"></iconify-icon>
+		</svelte:fragment>
+
+		<svelte:fragment slot="data">{totalChildren}</svelte:fragment>
+
+		<svelte:fragment slot="dataDescription">Children waiting for gifts</svelte:fragment>
+	</Card>
+
+	<Card>
+		<svelte:fragment slot="title">Total Weight</svelte:fragment>
+
+		<svelte:fragment slot="icon">
+			<iconify-icon icon="majesticons:scale-light" width="24" class="text-red-500"></iconify-icon>
+		</svelte:fragment>
+
+		<svelte:fragment slot="data">{totalWeight}</svelte:fragment>
+
+		<svelte:fragment slot="dataDescription">Total Weight of gifts for the children</svelte:fragment>
+	</Card>
+
+	<Card>
+		<svelte:fragment slot="title">Trips Created</svelte:fragment>
+
+		<svelte:fragment slot="icon">
+			<iconify-icon icon="material-symbols:travel" class="text-blue-500" width="30"></iconify-icon>
+		</svelte:fragment>
+
+		<svelte:fragment slot="data">{tripsCreated}</svelte:fragment>
+
+		<svelte:fragment slot="dataDescription">Trips created by you</svelte:fragment>
+	</Card>
+</section>
